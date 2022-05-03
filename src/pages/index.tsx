7b4dadb818967150c32ff8966210c787a2df61e8
Victor Duarte <victorctii217@gmail.com>
2022-05-03 16:33:03 -0300

    Tailwind CSS - Creating app's skeleton

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Layout from "../components/Layout";
+
 export default function Home() {
   return (
-    <div className={``}>
-      
+    <div className={`
+      flex justify-center items-center h-screen
+      bg-gradient-to-r from-purple-900 to to-blue-400
+      text-white
+    `}>
+      <Layout title='Registration'>
+        <span>Content</span>
+      </Layout>
     </div>
   )
 }
