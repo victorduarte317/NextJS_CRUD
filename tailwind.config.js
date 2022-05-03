d0bbee61b58fec763ef1ee6064721a59e68f64b6
Victor Duarte <victorctii217@gmail.com>
2022-05-03 16:14:05 -0300

    First commit - project setup

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}'
+  ],
+
+  darkMode: false, 
+  // content: [],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
