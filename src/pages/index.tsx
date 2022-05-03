e6a8da9c70ec5d472ef066a977aea50285a5578d
Victor Duarte <victorctii217@gmail.com>
2022-05-03 17:28:41 -0300

    Finished table component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,25 @@
 import Layout from "../components/Layout";
+import Table from '../components/Table';
+import Client from '../core/Client';
 
 export default function Home() {
+
+  const clients = [
+    new Client('John Doe', 34, '1'),
+    new Client('Anna Doe', 22, '2'),
+    new Client('Peter Doe', 57, '3'),
+    new Client('Joe Doe', 19, '4')
+  ]
+
+  function selectedClient(client: Client) {
+    console.log(client.name)
+  }
+
+  function deletedClient(client: Client) {
+    console.log(`Deleting ${client.name}`)
+  }
+
+
   return (
     <div className={`
       flex justify-center items-center h-screen
@@ -8,7 +27,10 @@ export default function Home() {
       text-white
     `}>
       <Layout title='Registration'>
-        <span>Content</span>
+        <Table clients={clients} 
+          selectedClient={selectedClient}
+          deletedClient={deletedClient}
+        />
       </Layout>
     </div>
   )
